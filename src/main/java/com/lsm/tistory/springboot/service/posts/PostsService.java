87c7caa6f2ff163cd58b4940ecfc122ff736edcf
inthegun <semin129@gmail.com>
2022-06-05 15:34:51 +0900

    등록/수정/조회 API 만들기 p.118

--- src/main/java/com/lsm/tistory/springboot/service/posts/PostsService.java
+++ src/main/java/com/lsm/tistory/springboot/service/posts/PostsService.java
@@ -1,7 +1,11 @@
 package com.lsm.tistory.springboot.service.posts;
 
+import com.lsm.tistory.springboot.domain.posts.Posts;
 import com.lsm.tistory.springboot.domain.posts.PostsRepository;
+import com.lsm.tistory.springboot.web.dto.PostsResponseDto;
 import com.lsm.tistory.springboot.web.dto.PostsSaveRequestDto;
+import com.lsm.tistory.springboot.web.dto.PostsUpdateRequestDto;
+import javafx.geometry.Pos;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +19,27 @@ public class PostsService {
     public long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id , PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow( () -> new
+                                    IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return  id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow( () -> new
+                                    IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    /**
+     * update 기능에서 데이터베이스에 쿼리를 날리는 부분이 없다.
+     * 이게 가능한 이유는 JPA의 영속성 컨텍스트 떄문
+     * 영속성 컨텍스트란 : 엔티티를 영구 저장하는 환경
+     */
 }
 
 /**
